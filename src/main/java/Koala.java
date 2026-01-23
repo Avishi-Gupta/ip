92f705de79622a8c982c5a7f1e4be1fff7afb5f8
Avishi-Gupta <e1355071@u.nus.edu>
2026-01-23 15:10:12 +0800

    Level-0 complete

---
+++ src/main/java/Koala.java
@@ -0,0 +1,5 @@
+public class Koala {
+    public static void main(String[] args) {
+        System.out.println("Hello from Koala!\nWhat can I do for you?\nBye. Hope to see you again soon!");
+    }
+}
