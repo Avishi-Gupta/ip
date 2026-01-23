351d40abeb6ee812a87615136e090573982be0fb
Avishi-Gupta <e1355071@u.nus.edu>
2026-01-23 15:53:29 +0800

    Level-1 complete

--- src/main/java/Koala.java
+++ src/main/java/Koala.java
@@ -1,5 +1,13 @@
 public class Koala {
     public static void main(String[] args) {
-        System.out.println("Hello from Koala!\nWhat can I do for you?\nBye. Hope to see you again soon!");
+        System.out.println("Hello from Koala!\nWhat can I do for you?\n");
+        if (args.length > 0 && args[0].equals("bye")) {
+            System.out.println("Hope to see you again soon!\n");
+        } else {
+            for (String arg : args) {
+                System.out.println(arg);
+            }
+        }
     }
 }
+
