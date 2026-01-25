7799fc986f89d0e289c7f09a4dfcd2ecee7e461d
Avishi-Gupta <e1355071@u.nus.edu>
2026-01-25 22:19:03 +0800

    Level-2 complete

--- src/main/java/Koala.java
+++ src/main/java/Koala.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Koala {
     public static void main(String[] args) {
-        System.out.println("Hello from Koala!\nWhat can I do for you?\n");
-        if (args.length > 0 && args[0].equals("bye")) {
-            System.out.println("Hope to see you again soon!\n");
-        } else {
-            for (String arg : args) {
-                System.out.println(arg);
+        try (Scanner scanner = new Scanner(System.in)) {
+            String[] msg = new String[100];
+            int msgCount = 0;
+
+            System.out.println("Hello! I'm Koala");
+            System.out.println("What can I do for you?");
+            
+            while (true) {
+                String input = scanner.nextLine();
+                
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                }
+
+                if (input.equals("list")) {
+                    for (int i = 0; i < msgCount; i++) {
+                        System.out.println((i + 1) + ". " + msg[i]);
+                    }
+                    continue;
+                }
+                
+                msg[msgCount] = input;
+                msgCount++;
+                System.out.println("added: " + input);
             }
         }
     }
 }
-
